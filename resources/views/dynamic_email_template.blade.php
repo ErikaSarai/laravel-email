cf2de3743e0ea7d4e8129191bcc33813d25bafff
Erika <lindanatera2013@gmail.com>
2019-08-14 16:49:03 -0400

    primer commit

---
+++ resources/views/dynamic_email_template.blade.php
@@ -0,0 +1,4 @@
+<p>Hola, soy {{ $data['name'] }}</p>
+<p>Email: {{ $data['email'] }}.</p>
+<p>Queria preguntarle: {{ $data['message'] }}.</p>
+<p>It would be appriciative, if you gone through this feedback.</p>
